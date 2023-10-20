bab3e09b2480fc90c57cd3eedb19a8d02b2e60e0
ASSIMv3 <hassanassim01@gmail.com>
2023-10-20 07:37:05 -0700

    mandatory task 8 error fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -17,8 +17,10 @@ def close(request):
 @app.route('/cities_by_states')
 def cities():
     """display a HTML page"""
-    states = [state for state in storage.all(State).values()]
-    return render_template('8-cities_by_states.html', states=states)
+    states = storage.all(State)
+    cities = storage.all(City)
+    return render_template('8-cities_by_states.html', states=states,
+                           cities=cities)
 
 
 if __name__ == '__main__':
