51773f41a307b786f52d3c9f327edac26a97b0a5
ASSIMv3 <hassanassim01@gmail.com>
2023-10-21 03:40:14 -0700

    adv task 100

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def close(error):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Displays the main HBnB filters HTML page."""
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    places = storage.all(Place)
+    return render_template('100-hbnb.html', states=states,
+                           amenities=amenities, places=places)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
